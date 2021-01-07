0634852f36827e3df6ce6dd64afe96ed85ec46f0
Martynas <nbtsst@gmail.com>
2021-01-07 23:51:54 +0200

    Lombok refactoring

--- src/main/java/learn/spring/martynas/services/RecipeServiceImpl.java
+++ src/main/java/learn/spring/martynas/services/RecipeServiceImpl.java
@@ -2,10 +2,12 @@ package learn.spring.martynas.services;
 
 import learn.spring.martynas.domain.Recipe;
 import learn.spring.martynas.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
     @Override
     public Set<Recipe> getRecipes() {
         Set<Recipe> recipeSet = new HashSet<>();
+        log.info("Recipes retrieved.");
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
