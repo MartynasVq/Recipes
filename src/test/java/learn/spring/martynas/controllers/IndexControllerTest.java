bb6ebacfb1b57008ba23f6bba209c0736f23b405
Martynas <58885736+MartynasVq@users.noreply.github.com>
2021-01-10 20:06:13 +0200

    Index Controller unit testing

--- src/test/java/learn/spring/martynas/controllers/IndexControllerTest.java
+++ src/test/java/learn/spring/martynas/controllers/IndexControllerTest.java
@@ -1,17 +1,21 @@
 package learn.spring.martynas.controllers;
 
+import learn.spring.martynas.domain.Recipe;
 import learn.spring.martynas.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 //
@@ -30,11 +34,28 @@ class IndexControllerTest {
     @Test
     void getIndexPage() {
 
+        //given
+        Set<Recipe> recipeSet = new HashSet<>();
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(2L);
+        recipeSet.add(recipe);
+        recipeSet.add(recipe1);
 
+        //when
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //then
         assertEquals("index", indexController.getIndexPage(model));
 
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        //verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute((eq("recipes")), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
 
 
     }
